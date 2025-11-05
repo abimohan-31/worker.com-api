73e19d8db8d278a124137640e8cd0f5a84dff41f
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-11-05 17:34:46 +0530

    modified

--- models/User.js
+++ models/User.js
@@ -1,42 +1,35 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    minlength: [8, "Password must be at least 8 characters"],
-    maxlength: [15, "Password cannot exceed 15 characters"],
-    unique: true,
-  },
-  phone: {
-    type: String,
-    required: [true, "Phone number is required"],
-  },
-  role: {
-    type: String,
-    enum: ["customer", "provider", "admin"],
-    default: "customer",
-  },
-  address: {
-    type: String,
-    required: [true, "Address is required"],
-  },
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password must be at least 8 characters"],
+      maxlength: [15, "Password cannot exceed 15 characters"],
+      unique: true,
+    },
 
-  created_at: {
-    type: Date,
-    default: Date.now,
+    role: {
+      type: String,
+      enum: ["customer", "provider", "admin"],
+      default: "customer",
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("User", userSchema);
